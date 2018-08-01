064a851391ba2636988cc8a8723d5d3a20f39330
whit2388 <35542960+whit2388@users.noreply.github.com>
2018-07-31 20:37:10 -0600

    Create scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+
+$(document).ready(function() {
+	console.log('Hello world!');
+
+	$('.card-body').hover( function() {
+			$(this).find('.img-title').fadeIn(300);
+		}, function() {
+			$(this).find('.img-title').fadeOut(100);
+		});
+	
+	$('#characterLeft').text('250 characters left');
+    $('#message').keydown(function () {
+        var max = 250;
+        var len = $(this).val().length;
+        if (len >= max) {
+            $('#characterLeft').text('You have reached the limit');
+            $('#characterLeft').addClass('red');
+            $('#btnSubmit').addClass('disabled');            
+        } 
+        else {
+            var ch = max - len;
+            $('#characterLeft').text(ch + ' characters left');
+            $('#btnSubmit').removeClass('disabled');
+            $('#characterLeft').removeClass('red');            
+        }
+    });    
+});
