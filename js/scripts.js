ba0ad4e23e424fbbb12ef6449ee6ecec0522e11a
whit2388 <35542960+whit2388@users.noreply.github.com>
2018-08-09 14:25:01 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 
 $(document).ready(function() {
 	console.log('Hello world!');
+	
+	
+  //Check to see if the window is top if not then display button
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500) {
+            $('.scrollToTop').fadeIn();
+        } else {
+            $('.scrollToTop').fadeOut();
+        }
+    });
+
+    //Click event to scroll to top
+    $('.scrollToTop').click(function(){
+        $('html, body').animate({scrollTop : 0},500);
+        return false;
+    });
 
 	$('.card-body').hover( function() {
 			$(this).find('.img-title').fadeIn(300);
