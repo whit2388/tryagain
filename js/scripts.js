962ca0d0fcf1a98b09e33f19e31300421757a846
whit2388 <35542960+whit2388@users.noreply.github.com>
2018-08-10 19:41:27 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,7 @@
 $(document).ready(function() {
 	console.log('Hello world!');
 	
-	
-  //Check to see if the window is top if not then display button
+	  //Check to see if the window is top if not then display button
     $(window).scroll(function(){
         if ($(this).scrollTop() > 500) {
             $('.scrollToTop').fadeIn();
@@ -17,6 +16,16 @@ $(document).ready(function() {
         $('html, body').animate({scrollTop : 0},500);
         return false;
     });
+	
+	
+	$(window).scroll(function() {
+	   if($(window).scrollTop() + $(window).height() > $(document).height() - 400) {
+		   $('.scrollToTop').addClass('fix-to-bottom');
+	   }else{
+		   $('.scrollToTop').removeClass('fix-to-bottom');
+	   }
+	});
+
 
 	$('.card-body').hover( function() {
 			$(this).find('.img-title').fadeIn(300);
@@ -41,42 +50,11 @@ $(document).ready(function() {
         }
     });
 	
-	// $('a').click(function(){
-    // $('html, body').animate({
-    //     scrollTop: $( $(this).attr('href') ).offset().top
-    // }, 500);
-    // return false;
-    // });
-
-    $('a[href*="#"]')
-        // Remove links that don't actually link to anything
-        .not('[href="#"]')
-        .not('[href="#0"]')
-        .click(function(event) {
-            // On-page links
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-                // Figure out element to scroll to
-                var target = $(this.hash);
-                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-                // Does a scroll target exist?
-                if (target.length) {
-                    // Only prevent default if animation is actually gonna happen
-                    event.preventDefault();
-                    $('html, body').animate({
-                        scrollTop: target.offset().top
-                    }, 1000, function() {
-                        // Callback after animation
-                        // Must change focus!
-                        // var $target = $(target);
-                        // $target.focus();
-                        // if ($target.is(":focus")) { // Checking if the target was focused
-                        //     return false;
-                        // } else {
-                        //     $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                        //     $target.focus(); // Set focus again
-                        // };
-                    });
-                }
-            }
-        });
+	$('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+	});
+	
 });
